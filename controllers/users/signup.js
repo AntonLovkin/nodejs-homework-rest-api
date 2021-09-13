b362a8dc0481eb39966d1460aa3318a0ff016bd8
AntonLovkin <72025158+AntonLovkin@users.noreply.github.com>
2021-09-13 12:35:27 +0300

    Update signup.js

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -12,10 +12,7 @@ const signup = async (req, res, next) => {
 
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
     const result = await User.create({ email, password: hashPassword })
-    // const newUser = new User({ email })
-    // newUser.setPassword(password)
-    // await newUser.save()
-
+   
     res.status(201).json({
       status: 'Created',
       code: 201,
