1a3399f28a8a604548b2e6ab38ef28b53d07f9df
AntonLovkin <anton28111987@gmail.com>
2021-09-19 16:38:29 +0300

    hw done

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,10 +2,12 @@ const signup = require('./signup')
 const login = require('./login')
 const logout = require('./logout')
 const current = require('./current')
+const updateImg = require('./updateImg')
 
 module.exports = {
   signup,
   login,
   logout,
-  current
+  current,
+  updateImg
 }
--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const gravatar = require('gravatar')
 const { Conflict } = require('http-errors')
 const { User } = require('../../models')
 
@@ -9,16 +10,17 @@ const signup = async (req, res, next) => {
     if (user) {
       throw new Conflict('Email in use')
     }
-
+    const defaultAvatar = gravatar.url(email)
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    const result = await User.create({ email, password: hashPassword })
-   
+    const result = await User.create({ email, password: hashPassword, avatarURL: defaultAvatar })
+
     res.status(201).json({
       status: 'Created',
       code: 201,
       user: {
-        email,
-        subscription: 'starter'
+        email: result.email,
+        subscription: 'starter',
+        avatarURL: result.avatarURL
       }
     })
   } catch (error) {
